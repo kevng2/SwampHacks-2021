179e0915b9070f1f4d1ce84cf2248b0d4df23366
Kevin Nguyen <nguyenkt6123@gmail.com>
2021-01-29 23:29:23 -0500

    added nav bar with naviagation components and fragments

--- app/src/main/java/com/android/kevng2/freestuff/MainActivity.java
+++ app/src/main/java/com/android/kevng2/freestuff/MainActivity.java
@@ -1,14 +1,30 @@
 package com.android.kevng2.freestuff;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationMenu;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
+
 public class MainActivity extends AppCompatActivity {
+    private BottomNavigationView mBottomNavigationView;
+    private NavHostFragment mNavHostFragment;
+    private NavController mNavController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mBottomNavigationView = findViewById(R.id.bottomNavigationView);
+        mNavHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.fragment);
+        if (mNavHostFragment != null)
+            mNavController =  mNavHostFragment.getNavController();
+        NavigationUI.setupWithNavController(mBottomNavigationView, mNavController);
     }
 }
