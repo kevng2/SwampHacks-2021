76015cf664b4c395b24906347ab06834ea4467e9
Cathy Yue <cy18@my.fsu.edu>
2021-01-31 02:37:42 -0500

    Uploading image in listing fragment

--- app/src/main/java/com/android/kevng2/freestuff/fragments/ListingFragment.java
+++ app/src/main/java/com/android/kevng2/freestuff/fragments/ListingFragment.java
@@ -1,10 +1,23 @@
 package com.android.kevng2.freestuff.fragments;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.media.Image;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,15 +25,23 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.android.kevng2.freestuff.R;
+
+import java.io.File;
+import java.io.InputStream;
 
 import timber.log.Timber;
 
 public class ListingFragment extends Fragment {
 
+    private static final int RESULT_OK = 0;
     ImageView ivPhoto;
     Button btnUpload;
+
+    private static final int REQUEST_CODE_STORAGE_PERMISSION = 1;
+    private static final int REQUEST_CODE_SELECT_IMAGE = 2;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -40,8 +61,75 @@ public class ListingFragment extends Fragment {
         spinner.setAdapter(conditions);
 
         //Uploading the image
+        ivPhoto = view.findViewById(R.id.ivPhoto);
         btnUpload = view.findViewById(R.id.btnUpload);
 
+        btnUpload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(ContextCompat.checkSelfPermission(getContext(), Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+                    ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, REQUEST_CODE_STORAGE_PERMISSION);
+                } else{
+                    selectImage();
+                }
+            }
+        });
+
+
         return view;
+    }
+    private void selectImage(){
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        if(intent.resolveActivity(getActivity().getPackageManager()) != null){
+            startActivityForResult(intent, REQUEST_CODE_SELECT_IMAGE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == REQUEST_CODE_STORAGE_PERMISSION && grantResults.length>0){
+            if(grantResults[0]==PackageManager.PERMISSION_GRANTED){
+                selectImage();
+            }else{
+                Toast.makeText(getContext(),"Permission Denied", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == REQUEST_CODE_SELECT_IMAGE && resultCode == RESULT_OK){
+            if(data != null){
+                Uri selectedImageUri = data.getData();
+                if(selectedImageUri != null){
+                    try {
+                        InputStream inputStream = getActivity().getContentResolver().openInputStream(selectedImageUri);
+                        Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+                        ivPhoto.setImageBitmap(bitmap);
+
+                        File selectedImage = new File(getPathFromUri(selectedImageUri));
+
+                    }catch (Exception exception){
+                        Toast.makeText(getContext(),exception.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        }
+    }
+
+    private String getPathFromUri(Uri contentUri){
+        String filePath;
+        Cursor cursor = getActivity().getContentResolver().query(contentUri,null,null,null,null);
+        if(cursor == null){
+            filePath = contentUri.getPath();
+        }else{
+            cursor.moveToFirst();
+            int index = cursor.getColumnIndex("_data");
+            filePath = cursor.getString(index);
+            cursor.close();
+        }
+        return filePath;
     }
 }
